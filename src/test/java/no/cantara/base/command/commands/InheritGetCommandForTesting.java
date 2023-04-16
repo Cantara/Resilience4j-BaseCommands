d21258c155eaeeee72f3f8cf7593fd8fcb79638e
Bjørn Viggo Andersen <bjorn@exoreaction.com>
2023-04-16 21:58:41 +0200

    Created test that now fails because of bug

---
+++ src/test/java/no/cantara/base/command/commands/InheritGetCommandForTesting.java
@@ -0,0 +1,46 @@
+package no.cantara.base.command.commands;
+
+import no.cantara.base.command.BaseHttpGetResilience4jCommand;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.Set;
+
+public class InheritGetCommandForTesting extends BaseHttpGetResilience4jCommand {
+    private final static String PATH = "/inherit-get-command-for-testing/";
+    private final static String GROUP_KEY = "RealEstateSetupSDK-GetRealEstate";
+    private String id;
+    private String bearerToken;
+
+    protected InheritGetCommandForTesting(URI baseUri, Map<String,String> extraHeaders) {
+        super(baseUri, GROUP_KEY, extraHeaders);
+    }
+
+    public InheritGetCommandForTesting(URI baseUri, String bearerToken, String id) {
+        this(baseUri, null);
+        this.bearerToken = bearerToken;
+        this.id = id;
+    }
+
+    public InheritGetCommandForTesting(URI baseUri, String bearerToken, String id, Map<String,String> extraHeaders) {
+        this(baseUri, extraHeaders);
+        this.bearerToken = bearerToken;
+        this.id = id;
+    }
+
+
+    @Override
+    protected URI buildUri() {
+        return URI.create(getBaseUri().toString() + PATH + id);
+    }
+
+    @Override
+    protected String buildAuthorization() {
+        return "Bearer " + this.bearerToken;
+    }
+
+    @Override
+    public Set<Integer> successfulStatusCodes() {
+        return Set.of(200,204);
+    }
+}
